735a84fe3705929b6ac540f8271a43fd59e8a68a
Rishav Sinha <rishavsinha2302@gmail.com>
2026-01-17 20:50:51 +0530

    Logo change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-card/80 backdrop-blur-sm">
       <div className="container flex h-16 items-center">
         <Link href="/" className="mr-4 flex items-center">
-          <Logo className="h-6 w-6" />
+          <Logo className="h-6" />
           <h1 className="ml-2 font-headline text-xl font-bold">CertiFire</h1>
         </Link>
       </div>
